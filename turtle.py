a413ff0de7903534a9b0a540c5fd900b71e2dcff
Mwaiyusuf <mwaigalo5@gmail.com>
2020-07-25 15:59:06 +0300

    turtle race game application

---
+++ turtle.py
@@ -0,0 +1,14 @@
+from turtle import Turtle 
+
+laura - Turtle()
+
+laura.color('red')
+laura.shape('turtle')
+
+laura.penup()
+laura.goto(-160,100)
+laura.pendown()
+
+rik = Turtle()
+lauren = Turtle()
+carrieanne = Turtle()
