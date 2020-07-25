304443ea9b056efcd51287fc379699f993506db0
Mwaiyusuf <mwaigalo5@gmail.com>
2020-07-25 16:34:20 +0300

    creatin an object

--- turtle.py
+++ turtle.py
@@ -1,10 +1,14 @@
 from turtle import Turtle 
+# importing from the class Turtle 
 
-laura - Turtle()
+# we will create an object called laura 
+laura =  Turtle()
 
+# we will assign the object attribute 
 laura.color('red')
 laura.shape('turtle')
 
+# we will have methods..which are the ones that tells the object what to do  
 laura.penup()
 laura.goto(-160,100)
 laura.pendown()
@@ -12,3 +16,24 @@ laura.pendown()
 rik = Turtle()
 lauren = Turtle()
 carrieanne = Turtle()
+
+rik.color('green')
+rik.shape('turtle')
+rik.penup()
+rik.goto(-160,70)
+rik.pendown()
+
+lauren.color('blue')
+lauren.shape('turtle')
+lauren.penup()
+lauren.goto(-160,40)
+lauren.pendown()
+
+carrieanne.color('purple')
+carrieanne.shape('turtle')
+carrieanne.penup()
+carrieanne.goto(-160,10)
+carrieanne.pendown()
+
+
+
